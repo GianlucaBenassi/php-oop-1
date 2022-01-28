14f9f585b0ad6640f30477ef45028c416fe0611e
Gianluca Benassi <benox1199@gmail.com>
2022-01-28 14:34:04 +0100

    construct

--- classes/Movie.php
+++ classes/Movie.php
@@ -6,6 +6,15 @@
         private $year;
         private $duration;
         private $price;
+
+        function __construct($_title, $_director, $_year, $_duration, $_price)
+        {
+            $this->title = $_title;
+            $this->director = $_director;
+            $this->year = $_year;
+            $this->duration = $_duration;
+            $this->price = $_price;
+        }
     }
 
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
     include_once __DIR__ . "./classes/Movie.php";
-    
+
+    $movies = [
+        new Movie("Spider-Man: No Way Home","Jon Watts",2021,148,10.00),
+        new Movie("Matrix Resurrections","Lana Wachowski",2021,148,8.50),
+        new Movie("Il Signore degli Anelli: La Compagnia dell'Anello","Peter Jackson",2001,178,10.00),
+        new Movie("Dune","Denis Villeneuve",2021,155,12.50),
+        new Movie("Space Jam: New Legends","Malcolm D. Lee",2021,115,9.00),
+        new Movie("Don't Look Up","Adam McKay",2021,145,8.50)
+    ];
+
+    var_dump($movies);
 
 ?>
